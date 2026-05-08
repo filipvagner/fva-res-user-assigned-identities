3414525ba9dff5faa59f32cb288ff6d84c03a82e
Filip Vagner <filip.vagner@orbit.cz>
2026-05-08 18:57:48 +0200

    Inintial module commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  description = "User assigned identity name"
+  type        = string
+}
+
+variable "location" {
+  description = "User assigned identity location"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "User assigned identity resource group name"
+  type        = string
+}
+
+variable "role_assignments" {
+  description = "Map of role assignments for the user assigned identity"
+  type = map(object({
+    role_definition_name = string
+    scope                = string
+  }))
+  default = {}
+}
