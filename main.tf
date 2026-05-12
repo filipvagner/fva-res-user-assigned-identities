fe489af2e18ff608ad739fe428d6aa3f6683ac99
Filip Vagner <filip.vagner@orbit.cz>
2026-05-12 20:52:47 +0200

    Added client id and principal id to store in kv

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+locals {
+  formatted_display_name = startswith(lower(var.name), "uai") ? var.name : "uai-${var.name}"
+}
+
 resource "azurerm_user_assigned_identity" "this" {
   location            = var.location
-  name                = var.name
+  name                = local.formatted_display_name
   resource_group_name = var.resource_group_name
 }
 
@@ -10,4 +14,18 @@ resource "azurerm_role_assignment" "this" {
   principal_id         = azurerm_user_assigned_identity.this.principal_id
   scope                = each.value.scope
   role_definition_name = each.value.role_definition_name
+}
+
+resource "azurerm_key_vault_secret" "client_id" {
+  name         = "${local.formatted_display_name}-clientid"
+  value        = azurerm_user_assigned_identity.this.client_id
+  content_type = "Managed by Terraform."
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "principal_id" {
+  name         = "${local.formatted_display_name}-principalid"
+  value        = azurerm_user_assigned_identity.this.principal_id
+  content_type = "Managed by Terraform."
+  key_vault_id = var.key_vault_id
 }
